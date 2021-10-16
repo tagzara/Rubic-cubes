54d7a6d4632e3be72d8f60d119ec65291f31df56
tagzara <tagzara82@gmail.com>
2021-10-16 17:50:07 +0300

    add accessory model

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,13 @@ const express = require('express');
 
 const cubeController = require('./controllers/cubeController.js');
 const homeController = require('./controllers/homeController.js');
+const accessoryController = require('./controllers/accessoryControler.js');
 
 const router = express.Router();
 
 router.use(homeController);
 router.use('/cube', cubeController);
+router.use('/accessory', accessoryController);
 router.use('*', (req, res) => {
     res.render('404');
 });
