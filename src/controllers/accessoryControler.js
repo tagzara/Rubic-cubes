ffecb6250dff73c1425a05c4f1de6c63d5b62685
tagzara <tagzara82@gmail.com>
2021-10-16 18:46:17 +0300

    add accessoaries view and fix details page

--- src/controllers/accessoryControler.js
+++ src/controllers/accessoryControler.js
@@ -1,14 +1,15 @@
-const express = require('express');
+const router = require('express').Router();
 
-const router = express.Router();
+const accessoryService = require('../services/accessoryService.js');
 
 router.get('/create', (req, res) => {
     res.render('accessory/create');
 });
 
-router.post('/create', (req, res) => {
-    let accessory = req.body;
-    console.log(accessory);
+router.post('/create', async (req, res) => {
+    let {name, description, imageUrl} = req.body;
+
+    await accessoryService.create(name, description, imageUrl);
 
     res.redirect('/');
 });
