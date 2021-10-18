0b2e4c96a4f33320433164110d8eb19416fa8dc4
tagzara <tagzara82@gmail.com>
2021-10-18 10:28:57 +0300

    show accessories select option

--- src/controllers/cubeAccessoryController.js
+++ src/controllers/cubeAccessoryController.js
@@ -1,11 +1,13 @@
 const router = require('express').Router({ mergeParams: true });
 
 const cubeService = require('../services/cubeService.js');
+const accessoryService = require('../services/accessoryService.js');
 
 router.get('/add', async (req, res) => {
     let cube = await cubeService.getOne(req.params.cubeId);
+    let accessories = await accessoryService.getAll();
 
-    res.render('cube/addAccessory', { ...cube });
+    res.render('cube/addAccessory', { cube, accessories });
 });
 
 module.exports = router;
